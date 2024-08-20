16d0751a13a4e074b50bf64c6119566f1f1f1f45
Victor <alliuvictor@gmail.com>
2024-08-20 07:07:55 -0700

    commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,8 @@
  * Description: Prints all possible different combinations of three digits
  * Numbers must be separated by ", ", followed by a space
  * The three digits must be different
- * 012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1 and 2
+ * 012, 120, 102, 021, 201,
+ * 210 are considered the same combination of the three digits 0, 1 and 2
  * Print only the smallest combination of three digits
  * Numbers should be printed in ascending order, with three digits
  * Return: Always 0 (Success)
