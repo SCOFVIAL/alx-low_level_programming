253d55acbcf8a9bd2e1d6289d6b8d50e4f60ccba
Victor <alliuvictor@gmail.com>
2024-08-20 05:01:06 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,12 +11,12 @@
  */
 int main(void)
 {
-    int n;
+int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-    if (n > 0)
+ if (n > 0)
     {
         printf("%d is positive\n", n);
     }
@@ -29,5 +29,5 @@ int main(void)
         printf("%d is negative\n", n);
     }
 
-    return (0);
+return (0);
 }
