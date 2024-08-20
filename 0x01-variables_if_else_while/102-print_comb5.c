6fb9e6b089bcec3f5306997990237e264e7b47f3
Victor <alliuvictor@gmail.com>
2024-08-20 07:18:59 -0700

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,42 +1,20 @@
 #include <stdio.h>
-11;rgb:2828/2c2c/3434
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
- * Description: Prints all possible combinations of two two-digit numbers
- * The numbers should range from 0 to 99
- * The two numbers should be separated by a space
- * All numbers should be
- * printed with two digits. 1 should be printed as 01
- * The combination of
- * numbers must be separated by comma, followed by a space
- * The combinations of numbers should
- * be printed in ascending order
- * 00 01 and 01 00 are considered as the
- * same combination of the numbers 0 and 1
+ * Description: Description: Generates a random number and prints it
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i, j;
+    int n;
 
-for (i = 0; i < 100; i++)
-{
-for (j = i + 1; j < 100; j++)
-{
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
-putchar(' ');
-putchar((j / 10) + '0');
-putchar((j % 10) + '0');
-if (i != 98 || j != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
 
-return (0);
+    printf("%d\n", n);
+
+    return (0);
 }
