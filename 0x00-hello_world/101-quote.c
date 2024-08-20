c5b1bde2d15c21918bc751b24296b05c4e498cb3
Victor <alliuvictor@gmail.com>
2024-08-20 04:29:16 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
- *              to the standard error
+ *Description: Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *to the standard error
  * Return: Always 1 (Error)
  */
 int main(void)
