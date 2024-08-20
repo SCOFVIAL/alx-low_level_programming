3dbbf461dcf364a2daabdd53e555571b26b099b8
Victor <alliuvictor@gmail.com>
2024-08-20 03:34:52 -0700

    Initial commit

--- 0x00-hello_world/1-main.c
+++ 0x00-hello_world/1-main.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	printf("Holberton School");
-	return (0);
+    printf("Hello, World!\n");
+    return (0);
 }
