9212972650929de5bb94bf21f6348f219b828f63
Victor <alliuvictor@gmail.com>
2024-08-20 06:48:07 -0700

    commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints the lowercase alphabet in reverse, followed by a new line
+ *Description: Prints the lowercase alphabet in reverse, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
