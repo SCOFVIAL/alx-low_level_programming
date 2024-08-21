a96f460d2dc232605dadf83b17ac0e166812343b
Victor <alliuvictor@gmail.com>
2024-08-21 07:53:28 -0700

    commit

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the sum of the even-valued terms in the Fibonacci sequence
+ * main - finds and prints the sum of the
+ * even-valued terms in the Fibonacci sequence
  *        whose values do not exceed 4,000,000
  *
  * Return: Always 0 (Success)
